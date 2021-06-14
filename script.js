cb702f06ba30d5c0c1b29af8c1faec525d338d93
IMarkinI <marcosmarkin55@gmail.com>
2021-06-14 12:41:36 -0300

    TUDO NOVO
    TUDO NOVO

---
+++ script.js
@@ -0,0 +1,12 @@
+var botao = document.getElementById ("button");
+
+botao.addEventListener("mouseover", function() {
+
+    this.style.backgroundColor = "#1d3557";
+});
+
+botao.addEventListener("mouseout", function() {
+
+    this.style.backgroundColor = "#007ea7";
+});
+
